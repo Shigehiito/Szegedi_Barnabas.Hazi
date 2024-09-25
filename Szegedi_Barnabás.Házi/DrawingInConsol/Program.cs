7d47b7c6b9bc6ad4d441b5bb25525ccba28af46a
Shigehiito <csanyi.gergo05@gmail.com>
2024-09-26 01:24:27 +0200

    Updated the code with the required changes. -deletes previous characters -changes color with num-buttons -changes character with function buttons.

--- Szegedi_Barnabás.Házi/DrawingInConsol/Program.cs
+++ Szegedi_Barnabás.Házi/DrawingInConsol/Program.cs
@@ -1,85 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 
 class Program
 {
     static int x = 10, y = 10;
+    static int prevX = 10, prevY = 10;
     static ConsoleColor currentColor = ConsoleColor.White;
-    static List<(int x, int y, ConsoleColor color)> trail = new List<(int, int, ConsoleColor)>();
+    static char currentChar = '█';
+    static char prevChar = ' ';
+
+    static List<(int x, int y, char character, ConsoleColor color)> trail = new List<(int, int, char, ConsoleColor)>();
+
+
+    static Dictionary<ConsoleKey, char> charMapping = new Dictionary<ConsoleKey, char>
+    {
+        { ConsoleKey.F1, '█' },
+        { ConsoleKey.F2, '▓' },
+        { ConsoleKey.F3, '▒' },
+        { ConsoleKey.F4, '░' }
+    };
+
+    static Dictionary<ConsoleKey, ConsoleColor> colorMapping = new Dictionary<ConsoleKey, ConsoleColor>
+    {
+        { ConsoleKey.D1, ConsoleColor.Red },
+        { ConsoleKey.D2, ConsoleColor.Green },
+        { ConsoleKey.D3, ConsoleColor.Blue },
+        { ConsoleKey.D4, ConsoleColor.Yellow },
+        { ConsoleKey.D5, ConsoleColor.Cyan },
+        { ConsoleKey.D6, ConsoleColor.Magenta },
+        { ConsoleKey.D7, ConsoleColor.White },
+        { ConsoleKey.D8, ConsoleColor.Gray }
+    };
 
     static void Main()
     {
         Console.CursorVisible = false;
-        ConsoleKeyInfo keyInfo;
+
+        DrawCursor();
 
         while (true)
         {
-            DrawTrail();  
+            ConsoleKeyInfo input = Console.ReadKey(true);
 
-            if (Console.KeyAvailable)
+            bool positionChanged = false;
+
+            switch (input.Key)
             {
-                keyInfo = Console.ReadKey(true);
+                case ConsoleKey.UpArrow:
+                    if (y > 0) { prevY = y; prevX = x; y--; positionChanged = true; }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (y < Console.WindowHeight - 2) { prevY = y; prevX = x; y++; positionChanged = true; }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (x > 0) { prevY = y; prevX = x; x--; positionChanged = true; }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (x < Console.WindowWidth - 1) { prevY = y; prevX = x; x++; positionChanged = true; }
+                    break;
+                case ConsoleKey.Spacebar:
+                    trail.Add((x, y, currentChar, currentColor));
+                    DrawCharacter(x, y, currentChar, currentColor);
+                    break;
+                case ConsoleKey.Escape:
 
-                switch (keyInfo.Key)
-                {
-                    case ConsoleKey.UpArrow or ConsoleKey.W:
-                        if (y > 0) y--;
-                        break;
-                    case ConsoleKey.DownArrow or ConsoleKey.S:
-                        if (y < Console.WindowHeight - 1) y++;
-                        break;
-                    case ConsoleKey.LeftArrow or ConsoleKey.A:
-                        if (x > 0) x--;
-                        break;
-                    case ConsoleKey.RightArrow or ConsoleKey.D:
-                        if (x < Console.WindowWidth - 1) x++;
-                        break;
-                    case ConsoleKey.Spacebar:
-                        ChangeColor();
-                        break;
-                    case ConsoleKey.Escape:
-                        return;
-                }
-
-                trail.Add((x, y, currentColor));
+                    return;
             }
 
-            Thread.Sleep(50);
+            if (colorMapping.ContainsKey(input.Key))
+            {
+                currentColor = colorMapping[input.Key];
+            }
+
+            if (charMapping.ContainsKey(input.Key))
+            {
+                currentChar = charMapping[input.Key];
+            }
+
+            if (positionChanged)
+            {
+                ClearCursorArea(prevX, prevY);
+                DrawCursor();
+
+
+            }
         }
     }
 
-    static void DrawTrail()
+    static void DrawCharacter(int posX, int posY, char character, ConsoleColor color)
     {
-        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var stringChars = new char[1];
-        var random = new Random();
+        Console.SetCursorPosition(posX, posY);
+        Console.ForegroundColor = color;
+        Console.Write(character);
+    }
 
-        for (int i = 0; i < stringChars.Length; i++)
-        {
-            stringChars[i] = chars[random.Next(chars.Length)];
-        }
-
-        var finalString = new String(stringChars);
-
-        Console.Clear();
-
-        foreach (var (posX, posY, color) in trail)
-        {
-            Console.SetCursorPosition(posX, posY);
-            Console.ForegroundColor = color;
-            Console.Write(finalString);
-        }
-
+    static void DrawCursor()
+    {
         Console.SetCursorPosition(x, y);
         Console.ForegroundColor = currentColor;
-        Console.Write(finalString);
+        Console.Write('_');
+
+        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write($"Kurzor pozíció: ({x}, {y}) | Prev. Kurzor pozíció: ({prevX}, {prevY}) | Szín: {currentColor} | Karakter: {currentChar} ");
     }
 
-    static void ChangeColor()
+    static void ClearCursorArea(int posX, int posY)
     {
-        var colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
-        Random random = new Random();
-        currentColor = colors[random.Next(colors.Length)];
+        var existingTrail = trail.Find(t => t.x == posX && t.y == posY);
+        if (existingTrail != default)
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.ForegroundColor = existingTrail.color;
+            Console.Write(existingTrail.character);
+        }
+        else
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.Write(' ');
+        }
     }
 }
